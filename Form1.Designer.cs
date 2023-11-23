707ce3c8bd40d065b07c3f3bc3c700cc710c52e7
Adnan <adnan.albarazi.edu@rattvik.se>
2023-11-23 14:27:56 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Övning_9._7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbxUtdata = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxNivå = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(569, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Visa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbxUtdata
+            // 
+            this.tbxUtdata.Location = new System.Drawing.Point(275, 140);
+            this.tbxUtdata.Multiline = true;
+            this.tbxUtdata.Name = "tbxUtdata";
+            this.tbxUtdata.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbxUtdata.Size = new System.Drawing.Size(289, 235);
+            this.tbxUtdata.TabIndex = 1;
+            this.tbxUtdata.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(581, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nivå";
+            // 
+            // tbxNivå
+            // 
+            this.tbxNivå.Location = new System.Drawing.Point(570, 160);
+            this.tbxNivå.Name = "tbxNivå";
+            this.tbxNivå.Size = new System.Drawing.Size(54, 20);
+            this.tbxNivå.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbxNivå);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxUtdata);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbxUtdata;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxNivå;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning_9._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int nivå = int.Parse(tbxNivå.Text);
+            for (int i=1 ; i <= nivå; i++)
+            {
+                for (int j=1 ; j <=10; j++)
+                {
+                    tbxUtdata.AppendText(i * j + "\t"); //Här Beräknas produkten av i och j och läggs den till resultatet i en TextBox komponent med namnet tbxUtdata.
+                }
+                tbxUtdata.AppendText("\r\n");
+            }
+        }
+    }
+}
